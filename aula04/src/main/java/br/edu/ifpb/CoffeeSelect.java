bcc65882b2188b5d03f22ebc7cf9d844b40b6f52
Paulo Ewerton <pauloewerton@icloud.com>
2020-10-27 16:14:15 -0300

    Final commit.

--- aula04/src/main/java/br/edu/ifpb/CoffeeSelect.java
+++ aula04/src/main/java/br/edu/ifpb/CoffeeSelect.java
@@ -1,6 +1,9 @@
 package br.edu.ifpb;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -16,11 +19,31 @@ public class CoffeeSelect extends HttpServlet {
         throws IOException, ServletException {
             
             String intensity = request.getParameter("intensity");
-            CoffeeExpert expert = new CoffeeExpert();
-            List<String> result = expert.getBrands(intensity);
+            response.sendRedirect("https://duckduckgo.com/?q=" + intensity);
+     //       CoffeeExpert expert = new CoffeeExpert();
+     //       List<String> result = expert.getBrands(intensity);
 
-            request.setAttribute("styles", result);
-            RequestDispatcher view = request.getRequestDispatcher("result.jsp");
-            view.forward(request, response);
+     //       request.setAttribute("styles", result);
+     //       RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+     //       view.forward(request, response);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+            
+            response.setContentType("application/zip");
+            InputStream is = getServletContext().getResourceAsStream("/teste.zip");
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            OutputStream out = response.getOutputStream();
+
+            while ((read = is.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+
+            out.flush();
+            out.close();
     }
 }
